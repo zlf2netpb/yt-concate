0c9fd1313f5c8cdec53977c253abf15b4527cca0
zlf2netpb <46837196+zlf2netpb@users.noreply.github.com>
2020-11-06 19:52:55 +0800

    Successful got a api_key and setting

---
+++ yt_concate/main.py
@@ -0,0 +1,39 @@
+import urllib.request
+import json
+import os
+from yt_concate.setting import API_KEY
+
+print(API_KEY)
+
+CHANEL_ID = 'UCb0xfM3HGOsqPYNAocXXNAQ'
+
+
+def get_all_video_in_channel(channel_id):
+    # api_key = 'AIzaSyCwVz-v2gX2QRBsqpWdIE4A5v1roSJRCpU'
+
+    base_video_url = 'https://www.youtube.com/watch?v='
+    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+
+    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key,
+                                                                                                        channel_id)
+
+    video_links = []
+    url = first_url
+    while True:
+        inp = urllib.request.urlopen(url)
+        resp = json.load(inp)
+
+        for i in resp['items']:
+            if i['id']['kind'] == "youtube#video":
+                video_links.append(base_video_url + i['id']['videoId'])
+
+        try:
+            next_page_token = resp['nextPageToken']
+            url = first_url + '&pageToken={}'.format(next_page_token)
+        except KeyError:
+            break
+    return video_links
+
+
+# result = get_all_video_in_channel(CHANEL_ID)
+# print(len(result))
